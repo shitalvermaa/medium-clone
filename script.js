033fa2ccc97f1e2523e648259d5b2da2c56a1f7d
shitalvermaa <shitalverma678@gmail.com>
2026-06-29 12:47:45 +0530

    added write page and article editor

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
- const menuToggle = document.querySelector('.menu-toggle');
-        const leftSidebar = document.querySelector('.left-sidebar');
+const menuToggle = document.querySelector('.menu-toggle');
+const leftSidebar = document.querySelector('.left-sidebar');
 
-        if (menuToggle && leftSidebar) {
-            menuToggle.addEventListener('click', function() {
-                leftSidebar.classList.toggle('hide-sidebar');
-            });
-        } else {
-            console.log("JS Error: Elements not found");
-        }
-      const searchInput = document.querySelector(".search-box input");
-      const posts = document.querySelectorAll(".post-card");
+if (menuToggle && leftSidebar) {
+    menuToggle.addEventListener('click', function() {
+        leftSidebar.classList.toggle('hide-sidebar');
+    });
+} else {
+    console.log("JS Error: Elements not found");
+}
+
+
+const searchInput = document.querySelector(".search-box input");
+const posts = document.querySelectorAll(".post-card");
+
+if(searchInput){
 
 searchInput.addEventListener("input", function () {
 
@@ -36,3 +40,62 @@ searchInput.addEventListener("input", function () {
     });
 
 });
+
+}
+
+
+const writeBtn = document.getElementById("writeBtn");
+
+if(writeBtn){
+    writeBtn.addEventListener("click", function(){
+        window.location.href = "write.html";
+    });
+}
+
+
+const publishBtn = document.getElementById("publishBtn");
+
+if(publishBtn){
+
+publishBtn.addEventListener("click", function(){
+
+    let title = document.getElementById("storyTitle").value;
+    let content = document.getElementById("storyContent").value;
+
+    if(title === "" || content === ""){
+        alert("Please fill all fields");
+        return;
+    }
+
+    let article = {
+        title:title,
+        content:content
+    };
+
+    localStorage.setItem("article", JSON.stringify(article));
+
+    alert("Article Published");
+
+    window.location.href = "index.html";
+
+});
+
+const savedArticle = JSON.parse(localStorage.getItem("article"));
+
+if(savedArticle){
+
+    let article = document.createElement("div");
+
+    article.className = "post-card";
+
+    article.innerHTML = `
+        <h2 class="post-title">${savedArticle.title}</h2>
+        <p class="post-subtitle">${savedArticle.content}</p>
+        <p class="author-name">You</p>
+    `;
+
+    document.querySelector(".posts").prepend(article);
+
+}
+
+}
